09bba29f9024b7c2bd85fce88de8ccb1b35d0a7c
Pranay Mangal <mangal.pranay@gmail.com>
2023-11-15 16:37:47 -0500

    ai data changed

--- src/components/carousel/AidataAPI.js
+++ src/components/carousel/AidataAPI.js
@@ -56,6 +56,8 @@ if (response.ok){
 
 export function queryData2 (inputStatement)
 {// console.log(inputStatement)
+  // console.log(`You are a helpful assistant. Only use the functions you have been provided with. Your are given a statement like '${inputStatement}'. Create content of 20 words for 4 presentation slides based on this statement. You can give content about the effect of the statement, reason for the statement, the impact of the statement. Also give titles to all 4 slides. Last slide will be about reaching out to author, or following his account for more details. `)
+
   return new Promise( async (resolve,reject)=>{
     const requestData = {
       messages: [
@@ -68,24 +70,31 @@ export function queryData2 (inputStatement)
       ]
      
     };
+console.log('sending request ...  ')
 
-   const response =  fetch('https://api.llama-api.com/chat/completions', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer LL-0et5NA5GzSdQyquwLV1f8gKY1LGXOCldbHgHXlGXNAaYpeFWo1ylaPFDltMVsLmx' // Replace <token> with the actual token
-        },
-        body: JSON.stringify(requestData)
-      })
+try{
+  const response =  await fetch('https://api.llama-api.com/chat/completions', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer LL-0et5NA5GzSdQyquwLV1f8gKY1LGXOCldbHgHXlGXNAaYpeFWo1ylaPFDltMVsLmx' // Replace <token> with the actual token
+    },
+    body: JSON.stringify(requestData)
+  })
 
-      if (response.ok){
-        const data = await response.json();
-        console.log("response is ok",data)
-        resolve(data)
-    }else {
-        reject ("Request failed");
-    }
-       
+  if (response.ok){
+    const data = await response.json();
+    console.log("response is ok",data)
+    resolve(data)
+}else {
+    reject ("Request failed");
+}
+   
+}catch(error){
+  console.error("erro during fetch", error)
+  reject("error during fetch")
+}
+   
       
 
    })
