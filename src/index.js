1c22bd34d65d1f5537d6b25e32f5c68f051de667
Pranay Mangal <mangal.pranay@gmail.com>
2023-11-28 03:29:44 -0500

    nextUi added

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './app/Store.js';
+import {NextUIProvider} from '@nextui-org/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+     <NextUIProvider>
       <Provider store={store}>
     <App />
     </Provider>
+    </NextUIProvider>
   </React.StrictMode>
 );
 
